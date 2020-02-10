5576a96caaa32838cc0c3d2aefa21cdbe431ee68
Martin Hunt <martinseanhunt@gmail.com>
2020-02-10 14:30:44 +0000

    solutiuon for image loading component

--- src/components/util/tests/PreloadImg.test.js
+++ src/components/util/tests/PreloadImg.test.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import { act } from 'react-dom/test-utils'
 
 import { findByTest } from '../../../test/testUtils' 
 import PreloadImg from '../PreloadImg'
 
-const HTTP_SERVER_URL = process.env.HTTP_SERVER_URL || 'http://localhost:3080/'
-
 const defaultProps = {
-  src: HTTP_SERVER_URL + 'img/testImg.jpg',
+  src: 'someFakeImageUrl.jpg',
   loadingText: 'heloooo'
 }
 
@@ -20,35 +17,34 @@ const init = props => {
   return mount(<PreloadImg {...componentProps} />)
 }
 
-test('Renders the image component when loaded', async () => {
-  const wrapper = init()
+/*
+  NOTE: I ended up changing how the preloading component
+  was working in order to make testing the onload event 
+  more reliable and less brittle... 
+  I actually much prefer how the component is working now too.
+  It does the same thing in a much simpler way.
+  
+  Happy with this now!
+*/
 
-  /* 
-    NOTE: Waiting to give the image time to load because I can't listen  
-    for when the onload event is fired within the useEffect hook.
-    I don't know that this is the best solution as it's too reliant on a
-    local http server and I don't like having to waiting for the useEffect
-    to run and the image to have been loaded. 
-    But it's the only solution I have at this point
-  */
-  await act(async () => await new Promise((r) => setTimeout(r, 1000)))
+describe('when image is loaded', () => {
+  test('Shows the image', () => {
+    const wrapper = init()
+    const image = findByTest(wrapper, 'image')
+    image.simulate('load')
+    const node = findByTest(wrapper, 'image-loaded')
+    expect(node.exists()).toBe(true) 
+  })
 
-  // have to simulate something or we can't see the update 
-  // even after state change
-  wrapper.simulate('click')
-  const node = findByTest(wrapper, 'loaded')
-
-  if(!node.exists()) {
-    console.warn(`
-      PLEASE MAKE SURE THE TEST HTTP SERVER IS RUNNING WITH "npm run httpserver"
-      or this test will always fail as it relies on a locally served image. 
-      Running the two scripts concurrently from package.json causes issues with 
-      watching for updates
-    `)
-  }
-
-  expect(node.exists()).toBe(true) 
+  test('Hides loading component', () => {
+    const wrapper = init()
+    const image = findByTest(wrapper, 'image')
+    image.simulate('load')
+    const node = findByTest(wrapper, 'loading')
+    expect(node.exists()).toBe(false) 
+  })
 })
+
 
 describe('when loading', () => { 
   test('Loading component is rendered', () => {
